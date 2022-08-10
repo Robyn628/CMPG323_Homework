f7e61ab9e9d77061412fc317f9fb0a6a0d3e4ceb
34669205 <cait1234botha@gmail.com>
2022-08-11 00:35:37 +0200

    Added source code

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[MovieTable]
+(
+	[MovieID] INT NOT NULL PRIMARY KEY, 
+    [MovieName] NVARCHAR(50) NULL, 
+    [Genre] NVARCHAR(50) NULL, 
+    [Rating] INT NULL, 
+    [Price] DECIMAL(10, 2) NULL
+)
